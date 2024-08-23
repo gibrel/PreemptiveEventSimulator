5f0d0e633c68d36f3b7cd1c68315fad089718db5
Gibrel <gibrel@gmail.com>
2024-08-23 15:56:31 -0300

    created a loop to execute many tests

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-import helpers.creators as cre
-from entities.game import Game
+import tests.test1_match_sim as t1
+import tests.test2_insert_city_data as t2
+import tests.test3_insert_sports as t3
+import tests.test4_import_and_create as t4
+import tests.test5_search as t5
+import tests.test6_redundant_insertion as t6
+import tests.test7_list_with_lambda_filter as t7
+import tests.test8_distribution as t8
+import tests.test9_curses as t9
+import tests.test10_curses_colors as t10
 
-my_game = Game()
-cre.populate_game(my_game)
-print('Game is populated. Let\'s play!')
-print(my_game)
+tests = [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10]
+
+# Run tests
+print("#################################\n"
+      "#####     Running tests     #####\n"
+      "#################################\n")
+for test in tests:
+    test.test()
