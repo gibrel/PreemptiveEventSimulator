b18e9c30c5b4611d73dcc7f459c401e2f34eba08
Gibrel <gibrel@gmail.com>
2024-08-30 09:23:45 -0300

    inserted positions in enumeration

--- helpers/enums.py
+++ helpers/enums.py
@@ -1,6 +1,33 @@
 import enum
 
 
+class Position:
+    GK = 'Goalkeeper'
+    DL = 'Defender Left'
+    DR = 'Defender Right'
+    DC = 'Defender Center'
+    DML = 'Defensive Midfielder Left'
+    DMR = 'Defensive Midfielder Right'
+    DMC = 'Defensive Midfielder Center'
+    ML = 'Midfielder Left'
+    MR = 'Midfielder Right'
+    MC = 'Midfielder Center'
+    AML = 'Attacking Midfielder Left'
+    AMR = 'Attacking Midfielder Right'
+    AMC = 'Attacking Midfielder Center'
+    FL = 'Foward Left'
+    FR = 'Foward Right'
+    FC = 'Foward Center'
+    SW = 'Sweeper'
+    LWB = 'Left Wingback'
+    RWB = 'Right Wingback'
+    RB = 'Right Back'
+    LB = 'Left Back'
+    CB = 'Center Back'
+    LW = 'Left Wing'
+    RW = 'Right Wing'
+
+
 class Category:
     age_15_17 = "age_15_17"
     age_18_19 = "age_18_19"
