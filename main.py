f245825eef7e6c553f8732611aa65d6f59a7ce05
Gibrel <gibrel@gmail.com>
2024-09-13 11:17:46 -0300

    updated main to use Textual widgets

--- main.py
+++ main.py
@@ -1,8 +1,56 @@
-from examples.textual_timer import TextualTimerApp
+from textual import on
+from textual.app import App
+from textual.widgets import Footer, Header, Label, ListView, ListItem
+import helpers.creators as cre
+from entities.game import Game
+from textual.widgets import Static
+import entities.locations as loc
 
-def main():
-    TextualTimerApp().run()
+
+class LeftSidebar(ListView):
+    entity_list: list
+
+    def __init__(self, entity_list=[]):
+        super().__init__()
+        self.entity_list = entity_list
+        
+    def compose(self):
+        for entity in self.entity_list:
+            yield ListItem(Label(entity.name))
 
 
-if __name__ == '__main__':
-    main()
+class Body(Static):
+    entity_list: list
+
+    def __init__(self, entity_list=[]):
+        super().__init__()
+        self.entity_list = entity_list
+        
+    def compose(self):
+        for entity in self.entity_list:
+            yield Static(entity.name)
+
+
+class PreemptiveEventSimulatorApp(App):
+    BINDINGS = [
+        # (key, action_name, description)
+    ]
+    TITLE = "Preemptive Event Simulator v0.0.0z"
+    CSS_PATH = "main.tcss"
+    app_game: Game
+
+    def compose(self):
+        yield Header(show_clock=True)
+        yield LeftSidebar(self.app_game.lists(loc.Region))
+        yield Body(self.app_game.lists(loc.State))
+        yield Footer()
+
+    def initialize_data(self):
+        self.app_game = Game()
+        cre.populate_game(self.app_game)
+
+
+if __name__ == "__main__":
+    app = PreemptiveEventSimulatorApp()
+    app.initialize_data()
+    app.run()
