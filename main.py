669300d7c96e9537903d0bef452ed2623572852b
Gibrel <gibrel@gmail.com>
2024-10-07 13:56:20 -0300

    created select screen for city

--- main.py
+++ main.py
@@ -1,53 +1,76 @@
-from textual import on
-from textual.app import App
-from textual.widgets import Footer, Header, Label, ListView, ListItem
-import helpers.creators as cre
+from textual.app import App, ComposeResult
+from textual.containers import Container
+from textual.widgets import Footer, Header, Static, Tree
+# import helpers.creators as cre
 from entities.game import Game
-from textual.widgets import Static
 import entities.locations as loc
-
-
-class LeftSidebar(ListView):
-    entity_list: list
-
-    def __init__(self, entity_list=[]):
-        super().__init__()
-        self.entity_list = entity_list
-        
-    def compose(self):
-        for entity in self.entity_list:
-            yield ListItem(Label(entity.name))
-
-
-class Body(Static):
-    entity_list: list
-
-    def __init__(self, entity_list=[]):
-        super().__init__()
-        self.entity_list = entity_list
-        
-    def compose(self):
-        for entity in self.entity_list:
-            yield Static(entity.name)
 
 
 class PreemptiveEventSimulatorApp(App):
     BINDINGS = [
         # (key, action_name, description)
+        ("d", "toggle_dark", "Toggle dark mode"),
     ]
-    TITLE = "Preemptive Event Simulator v0.0.0z"
+    TITLE = "Preemptive Event Simulator v0.0.001"
     CSS_PATH = "main.tcss"
     app_game: Game
-
-    def compose(self):
-        yield Header(show_clock=True)
-        yield LeftSidebar(self.app_game.lists(loc.Region))
-        yield Body(self.app_game.lists(loc.State))
-        yield Footer()
+    locations_tree = None
+    main_body = None
 
     def initialize_data(self):
         self.app_game = Game()
-        cre.populate_game(self.app_game)
+        # cre.populate_game(self.app_game)
+
+    def compose(self) -> ComposeResult:
+        self.locations_tree = Tree(self.app_game.get_first(loc.Country).name)
+        self.main_body = Static('\n' + (self.TITLE + " ")* 10 + '\n\n', id="body")
+        yield Header(show_clock=True)
+        with Container(id="sidebar"):
+            yield self.locations_tree
+        with Container(id="main"):
+            yield self.main_body
+        yield Footer()
+
+    def on_mount(self):
+        pais: Tree[dict] = self.locations_tree
+        pais.root.expand()
+        for regiao in self.app_game.lists(loc.Region):
+            regiao_corrente = pais.root.add(regiao.name)
+            for estado in self.app_game.lists(
+                loc.State, lambda x: x.region.name == regiao.name
+            ):
+                estado_corrente = regiao_corrente.add(estado.name)
+                for cidade in self.app_game.lists(
+                    loc.City, lambda x: x.state().name == estado.name
+                ):
+                    estado_corrente.add_leaf(cidade.name, data=cidade)
+        self.query_one(Tree).focus()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
+
+    def on_tree_node_highlighted(self, event: Tree.NodeHighlighted) -> None:
+        node = event.node
+        print_message = f"Node highlighted: {node.label}"
+        print(print_message)
+        if node.data.__class__ is loc.City:
+            self.main_body.update(f'{node.data.full_print()}')
+
+    def on_tree_node_selected(self, event: Tree.NodeSelected) -> None:
+        node = event.node
+        print_message = f"Node selected: {node.label}"
+        print(print_message)
+
+    def on_tree_node_collapsed(self, event: Tree.NodeCollapsed) -> None:
+        node = event.node
+        print_message = f"Node collapsed: {node.label}"
+        print(print_message)
+
+    def on_tree_node_expanded(self, event: Tree.NodeExpanded) -> None:
+        node = event.node
+        print_message = f"Node expanded: {node.label}"
+        print(print_message)
 
 
 if __name__ == "__main__":
