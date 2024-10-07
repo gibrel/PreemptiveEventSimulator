cc1fd756c643ca811724d34a40dcce6ad7be0fe7
Gibrel <gibrel@gmail.com>
2024-10-07 13:55:01 -0300

    added full_print method for city

--- entities/locations.py
+++ entities/locations.py
@@ -7,6 +7,7 @@ class Country(Location):
     """
     Country entity class.
     """
+
     abbreviation: str
 
     def __init__(self, uid: int, name: str, abbreviation: str):
@@ -14,13 +15,14 @@ class Country(Location):
         self.abbreviation = abbreviation
 
     def __str__(self) -> str:
-        return f'{self.uid}\t|\t{self.name}\t|\t{self.abbreviation}'
+        return f"{self.uid}\t|\t{self.name}\t|\t{self.abbreviation}"
 
 
 class Region(Location):
     """
     Region entity class.
     """
+
     abbreviation: str
     country: Country
 
@@ -29,13 +31,14 @@ class Region(Location):
         self.abbreviation, self.country = abbreviation, country
 
     def __str__(self) -> str:
-        return f'{self.uid}\t|\t{self.name}'
+        return f"{self.uid}\t|\t{self.name}"
 
 
 class State(Location):
     """
     State entity class.
     """
+
     abbreviation: str
     region: Region
 
@@ -44,8 +47,10 @@ class State(Location):
         self.abbreviation, self.region = abbreviation, region
 
     def __str__(self) -> str:
-        return f'{self.uid}\t|\t{self.name}\t|\t{self.abbreviation}\t|\t{self.region.name}'
-    
+        return (
+            f"{self.uid}\t|\t{self.name}\t|\t{self.abbreviation}\t|\t{self.region.name}"
+        )
+
     def country(self) -> Country:
         return self.region.country
 
@@ -54,6 +59,7 @@ class MediumRegion(Location):
     """
     Medium region entity class.
     """
+
     state: State
 
     def __init__(self, uid: int, name: str, state: State):
@@ -61,11 +67,11 @@ class MediumRegion(Location):
         self.state = state
 
     def __str__(self) -> str:
-        return f'{self.uid}\t|\t{self.name}\t|\t{self.state.name}'
-    
+        return f"{self.uid}\t|\t{self.name}\t|\t{self.state.name}"
+
     def region(self) -> Region:
         return self.state.region
-    
+
     def country(self) -> Country:
         return self.state.region.country
 
@@ -74,6 +80,7 @@ class IntermediateRegion(Location):
     """
     Intermediate region entity class.
     """
+
     state: State
 
     def __init__(self, uid: int, name: str, state: State):
@@ -81,11 +88,11 @@ class IntermediateRegion(Location):
         self.state = state
 
     def __str__(self) -> str:
-        return f'{self.uid}\t|\t{self.name}\t|\t{self.state.name}'
-    
+        return f"{self.uid}\t|\t{self.name}\t|\t{self.state.name}"
+
     def region(self) -> Region:
         return self.state.region
-    
+
     def country(self) -> Country:
         return self.state.region.country
 
@@ -94,6 +101,7 @@ class MicroRegion(Location):
     """
     Micro region entity class.
     """
+
     medium_region: MediumRegion
 
     def __init__(self, uid: int, name: str, medium_region: MediumRegion):
@@ -101,14 +109,14 @@ class MicroRegion(Location):
         self.medium_region = medium_region
 
     def __str__(self) -> str:
-        return f'{self.uid}\t|\t{self.name}\t|\t{self.medium_region.name} - {self.state().abbreviation}'
+        return f"{self.uid}\t|\t{self.name}\t|\t{self.medium_region.name} - {self.state().abbreviation}"
 
     def state(self) -> State:
         return self.medium_region.state
-    
+
     def region(self) -> Region:
         return self.medium_region.state.region
-    
+
     def country(self) -> Country:
         return self.medium_region.state.region.country
 
@@ -117,6 +125,7 @@ class ImmediateRegion(Location):
     """
     Immediate region entity class.
     """
+
     intermediate_region: IntermediateRegion
 
     def __init__(self, uid: int, name: str, intermediate_region: IntermediateRegion):
@@ -124,15 +133,17 @@ class ImmediateRegion(Location):
         self.intermediate_region = intermediate_region
 
     def __str__(self) -> str:
-        return (f'{self.uid}\t|\t{self.name}\t|\t{self.intermediate_region.name}'
-                f' - {self.state().abbreviation}')
+        return (
+            f"{self.uid}\t|\t{self.name}\t|\t{self.intermediate_region.name}"
+            f" - {self.state().abbreviation}"
+        )
 
     def state(self) -> State:
         return self.intermediate_region.state
-    
+
     def region(self) -> Region:
         return self.intermediate_region.state.region
-    
+
     def country(self) -> Country:
         return self.intermediate_region.state.region.country
 
@@ -141,33 +152,66 @@ class City(Location):
     """
     City entity class.
     """
+
     inhabitants: dict
     max_teams: dict
     immediate_region: ImmediateRegion
     micro_region: MicroRegion
 
-    def __init__(self, uid: int, name: str, immediate_region: ImmediateRegion, micro_region: MicroRegion):
+    def __init__(
+        self,
+        uid: int,
+        name: str,
+        immediate_region: ImmediateRegion,
+        micro_region: MicroRegion,
+    ):
         super().__init__(uid, name)
         self.immediate_region, self.micro_region = immediate_region, micro_region
-        self.inhabitants = {Category.age_15_17: 0, Category.age_18_19: 0, Category.age_20_24: 0,
-                            Category.age_25_29: 0, Category.age_30_up: 0}
-        self.max_teams = {Category.age_15_17: 0, Category.age_18_19: 0, Category.age_20_24: 0,
-                          Category.age_25_29: 0, Category.age_30_up: 0}
+        self.inhabitants = {
+            Category.age_15_17: 0,
+            Category.age_18_19: 0,
+            Category.age_20_24: 0,
+            Category.age_25_29: 0,
+            Category.age_30_up: 0,
+        }
+        self.max_teams = {
+            Category.age_15_17: 0,
+            Category.age_18_19: 0,
+            Category.age_20_24: 0,
+            Category.age_25_29: 0,
+            Category.age_30_up: 0,
+        }
 
     def __str__(self) -> str:
-        return f'{self.uid}\t|\t{self.name}\t|\t{self.state().abbreviation}'
+        return f"{self.uid}\t|\t{self.name}\t|\t{self.state().abbreviation}"
+
+    def full_print(self) -> str:
+        return (
+            f"\n\t{self.name} - {self.state().abbreviation}\n\n"
+            + f"\t\tEstado:\t{self.state().name} - {self.state().abbreviation}\n"
+            + f"\t\tRegião Intermediária:\t{self.intermediate_region().name}\n"
+            + f"\t\tRegião Imediata:\t{self.immediate_region.name}\n"
+            + f"\t\tMeso Região:\t{self.medium_region().name}\n"
+            + f"\t\tMicro Região:\t{self.micro_region.name}\n\n"
+            + "\t\tPopulação:\n"
+            + f"\t\t\tDe  15 a 17:\t{self.inhabitants[Category.age_15_17]}\n"
+            + f"\t\t\tDe  18 a 19:\t{self.inhabitants[Category.age_18_19]}\n"
+            + f"\t\t\tDe  20 a 24:\t{self.inhabitants[Category.age_20_24]}\n"
+            + f"\t\t\tDe  25 a 29:\t{self.inhabitants[Category.age_25_29]}\n"
+            + f"\t\t\tAcima de 30:\t{self.inhabitants[Category.age_30_up]}\n\n"
+        )
 
     def intermediate_region(self) -> IntermediateRegion:
         return self.immediate_region.intermediate_region
-    
+
     def medium_region(self) -> MediumRegion:
         return self.micro_region.medium_region
 
     def state(self) -> State:
         return self.micro_region.medium_region.state
-    
+
     def region(self) -> Region:
         return self.micro_region.medium_region.state.region
-    
+
     def country(self) -> Country:
         return self.micro_region.medium_region.state.region.country
