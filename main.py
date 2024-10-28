45535dd746aa5178db5cb82cce1b0b94e0f59a1a
Gibrel <gibrel@gmail.com>
2024-10-28 10:20:33 -0300

    implemented select team screen

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 from __future__ import annotations
 from textual.app import App, ComposeResult
 from textual.containers import Container, Horizontal, Vertical
-from textual.widgets import Footer, Header, Static, Tree, Label
+from textual.widgets import Footer, Header, Static, Tree, Label, RadioButton, RadioSet
 import helpers.creators as cre
 from entities.game import Game
 import entities.locations as loc
 import entities.sports as spr
 
+
+GAME = Game()
+
+class RadioModule(Static):
+
+    def compose(self) -> ComposeResult:
+        yield Label("TIMES", id="radio_label")
+        yield RadioSet(id="radioSet")
+
+class BodyModule(Static):
+
+    def compose(self) -> ComposeResult:
+        yield Label("CIDADE", id="body_label")
+        yield Static("\n\tMussum Ipsum, cacilds vidis litro abertis.  Pra lá, depois divoltis porris, paradis. Mé faiz elementum girarzis, nisi eros vermeio. Delegadis gente finis, bibendum egestas augue arcu ut est. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.", id="content")
+
+class TreeModule(Static):
+
+    def compose(self) -> ComposeResult:
+        yield Label("LOCALIZAÇÃO", id="tree_label")
+        yield Tree(GAME.get_first(loc.Country).name, id="locations")
+
+class ScreenModule(Static):
+
+    def compose(self) -> ComposeResult:
+        with Horizontal():
+            yield TreeModule(id="tree")
+            yield BodyModule(id="body")
+            yield RadioModule(id="radio")
 
 class PreemptiveEventSimulatorApp(App):
     BINDINGS = [
@@ -15,65 +43,55 @@ class PreemptiveEventSimulatorApp(App):
     ]
     TITLE = "Preemptive Event Simulator v0.0.002"
     CSS_PATH = "main.tcss"
-    app_game: Game
 
     def initialize_data(self):
-        self.app_game = Game()
-        cre.populate_game(self.app_game)
+        cre.populate_game(GAME)
 
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
-        with Container(id="sidebar"):
-            yield Tree(self.app_game.get_first(loc.Country).name)
-        with Horizontal():
-            yield Static('\n' + (self.TITLE + " ")* 10 + '\n\n', id="body")
+        yield ScreenModule(id="screen")
         yield Footer()
-
-    def on_mount(self):
-        pais: Tree[dict] = self.query_one(Tree)
-        pais.root.expand()
-        for regiao in self.app_game.lists(loc.Region):
-            regiao_corrente = pais.root.add(regiao.name)
-            for estado in self.app_game.lists(
-                loc.State, lambda x: x.region.name == regiao.name
-            ):
-                estado_corrente = regiao_corrente.add(estado.name)
-                for cidade in self.app_game.lists(
-                    loc.City, lambda x: x.state().name == estado.name
-                ):
-                    estado_corrente.add_leaf(cidade.name, data=cidade)
-        pais.focus()
-
-    def refresh_teams(self, city: loc.City | None) -> None:
-        if city is None:
-            return
-        teams = self.app_game.lists(spr.Team, lambda x: x.city == city)
-        for team in teams:
-            self.selected_team = []
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
 
+    def on_mount(self):
+        pais: Tree[dict] = self.query_one(Tree)
+        pais.root.expand()
+        for regiao in GAME.lists(loc.Region):
+            regiao_corrente = pais.root.add(regiao.name)
+            for estado in GAME.lists(
+                loc.State, lambda x: x.region.name == regiao.name
+            ):
+                estado_corrente = regiao_corrente.add(estado.name)
+                for cidade in GAME.lists(
+                    loc.City, lambda x: x.state().name == estado.name
+                ):
+                    estado_corrente.add_leaf(cidade.name, data=cidade)
+        pais.focus()
+
     def on_tree_node_highlighted(self, event: Tree.NodeHighlighted) -> None:
         node = event.node
         if node.data.__class__ is loc.City:
-            body = self.query_one("#body")
+            body = self.query_one("#content")
             body.update(f'{self.print_city_data(node.data)}')
             self.refresh_teams(node.data)
 
     def print_city_data(self, city: loc.City) -> str:
         print = city.full_print()
-        teams = self.app_game.lists(spr.Team, lambda x: x.city == city)
-        print += f"\t\tTimes ({len(teams)}):\n"
-        i = 0
-        for team in teams:
-            print += f"\t\t\t{team.name}"
-            if i % 3 == 2:
-                print += f"{i}\n"
-            i += 1
-        print += "\n"
         return print
+
+    def refresh_teams(self, city: loc.City | None) -> None:
+        if city is not None:
+            teams = GAME.lists(spr.Team, lambda x: x.city == city)
+            radioSet = self.query_one(RadioSet)
+            radioSet.remove_children()
+            for team in teams:
+                radioSet.mount(RadioButton(team.name))
+
+    def on_radio_set_changed(self, event: RadioSet.Changed) -> None:
+        event.pressed.focus()
 
 
 if __name__ == "__main__":
